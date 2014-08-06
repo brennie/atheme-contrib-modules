ca4ed7566e3048632a86d0a3c705af3e846e08ea
Barret Rennie <barret@brennie.ca>
2014-08-05 19:17:56 -0600

    os_alert: Add IP criteria

--- os_alert.c
+++ os_alert.c
@@ -335,6 +335,33 @@ alert_criteria_constructor_t alert_host_criteria = {
 	EVT_CONNECT
 };
 
+static bool alert_ip_criteria_exec(user_t *u, alert_criteria_t *c)
+{
+	alert_pattern_criteria_t *criteria = (alert_pattern_criteria_t *)c;
+
+	return_val_if_fail(u != NULL, false);
+	return_val_if_fail(c != NULL, false);
+
+	return pattern_match(criteria->pattern, u->ip);
+}
+
+static void alert_ip_criteria_display(char *s, size_t size, alert_criteria_t *c)
+{
+	alert_pattern_criteria_t *criteria = (alert_pattern_criteria_t *)c;
+
+	return_if_fail(s != NULL);
+	return_if_fail(c != NULL);
+
+	snappendf(s, size, " IP");
+	pattern_display(s, size, criteria->pattern);
+}
+
+alert_criteria_constructor_t alert_ip_criteria = {
+	alert_pattern_criteria_prepare, alert_ip_criteria_exec, alert_pattern_criteria_cleanup,
+	alert_ip_criteria_display,
+	EVT_CONNECT
+};
+
 static alert_action_t *alert_notice_action_prepare(char **args)
 {
 	(void)args;
@@ -422,6 +449,7 @@ void _modinit(module_t *module)
 	mowgli_patricia_add(alert_cmdtree, "USER", &alert_user_criteria);
 	mowgli_patricia_add(alert_cmdtree, "GECOS", &alert_gecos_criteria);
 	mowgli_patricia_add(alert_cmdtree, "HOST", &alert_host_criteria);
+	mowgli_patricia_add(alert_cmdtree, "IP", &alert_ip_criteria);
 
 	alert_acttree = mowgli_patricia_create(strcasecanon);
 	mowgli_patricia_add(alert_acttree, "NOTICE", &alert_notice_action);
@@ -495,6 +523,7 @@ void _moddeinit(module_unload_intent_t intent)
 	mowgli_patricia_delete(alert_cmdtree, "USER");
 	mowgli_patricia_delete(alert_cmdtree, "GECOS");
 	mowgli_patricia_delete(alert_cmdtree, "HOST");
+	mowgli_patricia_delete(alert_cmdtree, "IP");
 	mowgli_patricia_destroy(alert_cmdtree, NULL, NULL);
 
 	mowgli_patricia_delete(alert_acttree, "NOTICE");
